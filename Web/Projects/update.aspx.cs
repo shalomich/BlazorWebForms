9a91b8ab9bcd2d11da0a65e05a3d06a90e1ef06f
Egor Doroshkevich <egor.doroshkevich@saritasa.com>
2025-02-21 13:09:39 +0500

    Add comment

--- Web/Projects/update.aspx.cs
+++ Web/Projects/update.aspx.cs
@@ -31,6 +31,7 @@ public partial class projects_update : System.Web.UI.Page
             return;
         }
 
+        // HACK: Assign raw URL to form instead of auto-generated one since it's incorrect.
         Form.Action = Request.RawUrl;
 
         var projectIdString = Request.QueryString["id"];
