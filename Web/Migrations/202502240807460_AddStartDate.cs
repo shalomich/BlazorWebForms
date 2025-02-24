48803b64437e8ae3b735170af6358af6b0784878
Egor Doroshkevich <egor.doroshkevich@saritasa.com>
2025-02-24 13:13:09 +0500

    Add start date column to database

---
+++ Web/Migrations/202502240807460_AddStartDate.cs
@@ -0,0 +1,18 @@
+﻿namespace WebForms.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStartDate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Projects", "StartDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Projects", "StartDate");
+        }
+    }
+}
