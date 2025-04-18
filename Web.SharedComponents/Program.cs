998be9951106e44bc041089e556571c5149b9640
Nikita Epishev <nikita.epishev@saritasa.com>
2025-04-18 09:19:10 +0400

    Display WebForms after blazor loading SN-787

--- Web.SharedComponents/Program.cs
+++ Web.SharedComponents/Program.cs
@@ -1,10 +1,9 @@
-using Web.SharedComponents.Components;
-using Web.SharedComponents.Infrastructure;
+using Common.DI;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Common.Web;
-using Common.DI;
+using Web.SharedComponents.Components;
 using Web.SharedComponents.Components.ProjectCreateDialog;
+using Web.SharedComponents.Infrastructure;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -17,5 +16,6 @@ builder.RootComponents.RegisterCustomElement<AppHeader>("app-header");
 builder.RootComponents.RegisterCustomElement<AppProvider>("app-provider");
 builder.RootComponents.RegisterCustomElement<ProjectCreateButton>("project-create-button");
 builder.RootComponents.RegisterCustomElement<AppDatePicker>("app-date-picker");
+builder.RootComponents.RegisterCustomElement<BlazorReadyNotifier>("blazor-ready");
 
 await builder.Build().RunAsync();
